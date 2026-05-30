d6aa60eeb3b4679f71910de6b0e5195272c3708d
Qbaldo <durowski@gmail.com>
2026-05-30 21:42:07 +0200

    solution

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'taxi_service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,3 +117,5 @@ AUTH_USER_MODEL = 'taxi.Driver'
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
